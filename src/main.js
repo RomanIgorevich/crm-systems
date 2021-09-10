b861afc371bc412ade5fddd1e7753fd534e5ce75
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-09-10 16:15:32 +0300

    добавление времени, фильтра

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/scss/styles.scss'
-import 'materialize-css/dist/js/materialize.min.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/scss/styles.scss";
+import "materialize-css/dist/js/materialize.min.js";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
+// фильтры
+app.config.globalProperties.$filters = {
+  date(value) {
+    const options = {};
+    options.day = "2-digit";
+    options.month = "long";
+    options.year = "numeric";
+    options.hour = "2-digit";
+    options.minute = "2-digit";
+    options.second = "2-digit";
+    return new Intl.DateTimeFormat("ru-RU", options).format(new Date(value));
+  },
+};
+
+app.use(router);
+app.mount("#app");
