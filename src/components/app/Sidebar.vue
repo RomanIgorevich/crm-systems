f36855c4d8559636948b5d730abc5c56da4c632b
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-09-10 14:07:07 +0300

    вынес sidebar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <li v-for="link in links" :key="link.url">
+      <router-link
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+        class="waves-effect waves-orange pointer"
+      >
+        {{ link.title }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss">
+.active{
+  background: #f1eff1;
+}
+</style>
